ca824f08773a6f5cbcf33e4f2b561f560006d756
kristen wong <kristenccwong@gmail.com>
2026-09-16 00:16:39 +0800

    rename pushforce

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,19 +2,21 @@ extends CharacterBody3D
 @onready var speed_label: Label = $Camera3D/debugLabel
 
 
-@export var ACCEL = 200
+@export var PUSH_FORCE = 800
 @export var DECCEL = 80
 
+# todo change fov
 
 func _physics_process(dt: float) -> void:
 	if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("action1"):
 		velocity.z = 0
-		velocity.z -= ACCEL * dt
+		velocity.z -= PUSH_FORCE * dt
 	elif position.z <= 50:
 		velocity.z += DECCEL * dt
 	else:
 		velocity.z = 0
 		
 	speed_label.text = "SPEED: "+str(velocity.z)+"m/s\n"+"Z: "+str(position.z)
+	print(PUSH_FORCE)
 
 	move_and_slide()
